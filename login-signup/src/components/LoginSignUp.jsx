ccafd9667f11935f636fa79840fe5e8fe4cf649e
Satyam Kumar <99055360+AirSatyam07@users.noreply.github.com>
2024-12-20 03:39:30 +0000

    first commit

---
+++ login-signup/src/components/LoginSignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LoginSignUp.css'
+
+function LoginSignUp() {
+  return (
+    <>
+    <div className='container'>
+        <div className="header">
+            <div className="text">SignUp</div>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default LoginSignUp
